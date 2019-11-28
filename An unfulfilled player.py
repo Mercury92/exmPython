c6e700eec67880486cc2e01d939ab74a018841ce
choishuning <cse555@naver.com>
2019-11-28 20:25:00 +0900

    완주하지 못한 선수

---
+++ An unfulfilled player.py
@@ -0,0 +1,11 @@
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    for a in range(len(completion)):
+        if participant[a] != completion[a]:
+            answer = participant[a]
+            break
+        if not a:
+            answer = participant[len(participant)-1]
+
+            return answer
