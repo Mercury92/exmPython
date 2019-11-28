fc0bf86b109d8176637ae8cb9a0ce731519d9033
choishuning <cse555@naver.com>
2019-11-28 20:30:25 +0900

    같은 숫자는 싫어_JAVA

---
+++ Don't want the same number.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Solution {
+	public int[] solution(int []arr) {
+        ArrayList <Integer> list = new ArrayList<Integer>();
+        int nowNum = 10;
+        for(int a=0; a<arr.length; a++){
+            if(arr[a] != nowNum){
+                list.add(arr[a]);
+                nowNum = arr[a];
+            }
+        }
+        int[] answer = new int[list.size()];
+        for(int b=0; b<list.size(); b++){
+            answer[b] = list.get(b);
+        }
+
+        return answer;
+	}
+}
