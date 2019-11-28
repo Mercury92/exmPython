b1490f729e8c01cefc37a9799e3ce6fe28415111
choishuning <cse555@naver.com>
2019-11-28 20:28:00 +0900

    가운데 글자 가져오기_JAVA

---
+++ Bring middle letter.java
@@ -0,0 +1,20 @@
+class Solution {
+  public String solution(String s) {
+      int a = s.length();
+      String answer = "";
+
+      if(a%2 == 1) {
+          int b = a/2;
+          answer = s.substring(b,b+1);
+      } else {
+          int b = a/2-1;
+          answer = s.substring(b,b+2);
+      }
+      return answer;
+    }
+    public static void  main(String[] args){
+        Solution s = new Solution();
+        System.out.println(s.solution("power"));
+        System.out.println(s.solution("test1234"));
+    }
+}
